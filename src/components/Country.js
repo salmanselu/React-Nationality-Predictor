e73f7c2d7668e40eb8d295b9601ac223b0271c86
=Salman nazeer <salmanselu2002@gmail.com>
2021-09-26 21:30:54 +0530

    Added country flags

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,7 +3,7 @@ import './Country.css'
 let Country = (props) => {
     return (
     <div className="country">
-        <span className="country-id">{props.countryId}</span>
+        <span className="country-id"><img src={`https://www.countryflags.io/${props.countryId}/shiny/32.png`}></img></span>
         <span className="percentage-bar"><progress value={props.percent*100} max="100"></progress></span>
         <span className="percentage-numeral">{`${Math.round(props.percent*100)}%`}</span>
 
